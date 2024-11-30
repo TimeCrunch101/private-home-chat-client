1256f17ef82282d2571b1c79ddc147badf54c96e
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2024-11-29 20:36:13 -0500

    chore: removing more bootstrap

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,3 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/js/bootstrap.js";
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
 import App from './App.vue';
